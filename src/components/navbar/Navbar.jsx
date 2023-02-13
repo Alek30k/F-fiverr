2bbf9005a5945e86671bbc228c183239eaaa34b9
Alek30k <alemank23k@gmail.com>
2023-02-13 17:19:58 -0300

    last commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,7 +9,14 @@ const Navbar = () => {
           <span className="text">fiverr</span>
           <span className="dot">.</span>
         </div>
-        <div className="links"></div>
+        <div className="links">
+          <span>Fiverr Business</span>
+          <span>Explore</span>
+          <span>English</span>
+          <span>Become a seller</span>
+          <span>Sign in</span>
+          <button>Join</button>
+        </div>
       </div>
     </div>
   );
