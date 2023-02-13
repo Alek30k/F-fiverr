3611b193743d910d84eb2f56cd9b30d1742401d9
Alek30k <alemank23k@gmail.com>
2023-02-13 17:28:48 -0300

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar />
     </div>
   );
