1ccd298eff54a7c37f4131b63fe6a26a65dc3e33
Alek30k <alemank23k@gmail.com>
2023-02-14 10:47:29 -0300

    last commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Navbar.scss";
 
 const Navbar = () => {
@@ -6,7 +7,9 @@ const Navbar = () => {
     <div className="navbar">
       <div className="container">
         <div className="logo">
-          <span className="text">fiverr</span>
+          <Link>
+            <span className="text">fiverr</span>
+          </Link>
           <span className="dot">.</span>
         </div>
         <div className="links">
