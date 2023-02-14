71940db40c193971d08dd5804978733392e301ff
Alek30k <alemank23k@gmail.com>
2023-02-14 16:45:09 -0300

    last commit

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from "react";
-// import { Link } from "react-router-dom";
+// import { useLocation } from "react-router-dom";
 import "./Navbar.scss";
 
 const Navbar = () => {
   const [active, setActive] = useState(false);
   const [open, setOpen] = useState(false);
 
+  // const { pathname } = useLocation();
+
   const isActive = () => {
     window.scrollY > 0 ? setActive(true) : setActive(false);
   };
+  console.log(open);
 
   useEffect(() => {
     window.addEventListener("scroll", isActive);
@@ -21,7 +24,7 @@ const Navbar = () => {
 
   const currentUser = {
     id: 1,
-    username: "Anna",
+    username: "Ale",
     isSeller: true,
   };
 
@@ -42,23 +45,25 @@ const Navbar = () => {
           <span>Sign in</span>
           {!currentUser && <button>Join</button>}
           {currentUser && (
-            <div className="user">
+            <div className="user" onClick={() => setOpen(!open)}>
               <img
                 src="https://user-images.githubusercontent.com/101005998/218835132-accead2a-fc7a-42e4-b11a-675246ea282b.png"
                 alt="imgUser not found"
               />
               <span>{currentUser?.username}</span>
-              <div className="options">
-                {currentUser?.isSeller && (
-                  <>
-                    <span>Gigs</span>
-                    <span>Add New Gig</span>
-                  </>
-                )}
-                <span>Orders</span>
-                <span>Messages</span>
-                <span>Logout</span>
-              </div>
+              {open && (
+                <div className="options">
+                  {currentUser?.isSeller && (
+                    <>
+                      <span>Gigs</span>
+                      <span>Add New Gig</span>
+                    </>
+                  )}
+                  <span>Orders</span>
+                  <span>Messages</span>
+                  <span>Logout</span>
+                </div>
+              )}
             </div>
           )}
         </div>
