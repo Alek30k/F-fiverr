f487600e350ef62a2bf0e38cb5abb09f7f731edd
Alek30k <alemank23k@gmail.com>
2023-02-15 15:44:36 -0300

    last commit

--- src/components/slide/Slide.jsx
+++ src/components/slide/Slide.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import "./Slide.scss";
+import Slider from "infinite-react-carousel";
+import CatCard from "../catCard/CatCard";
+import { cards } from "../../data";
 
 const Slide = () => {
   return (
     <div className="slide">
-      <div className="container">Slide</div>
+      <div className="container">
+        <Slider>
+          {cards.map((card) => (
+            <CatCard item={card} key={card.id} />
+          ))}
+        </Slider>
+      </div>
     </div>
   );
 };
