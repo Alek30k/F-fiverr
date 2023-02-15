1a2c0790042929f9f02430e1c751d7b544fb05e8
Alek30k <alemank23k@gmail.com>
2023-02-15 17:21:35 -0300

    last commit

--- src/components/slide/Slide.jsx
+++ src/components/slide/Slide.jsx
@@ -8,7 +8,7 @@ const Slide = ({ slidesToShow, arrowsScroll }) => {
   return (
     <div className="slide">
       <div className="container">
-        <Slider slidesToShow={5} arrowsScroll={5}>
+        <Slider slidesToShow={slidesToShow} arrowsScroll={arrowsScroll}>
           {cards.map((card) => (
             <CatCard item={card} key={card.id} />
           ))}
