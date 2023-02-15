f75274e8f9eae4573eb4f2a097fedb4a4dbd4d5a
Alek30k <alemank23k@gmail.com>
2023-02-15 10:55:06 -0300

    last commit

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -7,8 +7,24 @@ const Featured = () => {
       <div className="container">
         <div className="left">
           <h1>Find the perfect freelance servicio for your business</h1>
+          <div className="search">
+            <div className="searchInput">
+              <img src="" alt="" />
+              <input type="text" placeholder='Try "building mobil app"' />
+            </div>
+            <button>Search</button>
+          </div>
+          <div className="popular">
+            <span>Popular:</span>
+            <button>Web Design</button>
+            <button>WordPress</button>
+            <button>Logo Design</button>
+            <button>AI Services</button>
+          </div>
         </div>
-        <div className="right"></div>
+        <div className="right">
+          <img src="./img/man.png" alt="" />
+        </div>
       </div>
     </div>
   );
