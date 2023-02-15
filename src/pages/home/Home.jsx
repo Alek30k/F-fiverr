33e9f02c0fbcfc6bb41cd0a57f9412dd9874e135
Alek30k <alemank23k@gmail.com>
2023-02-15 15:13:19 -0300

    last commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Featured from "../../components/featured/Featured";
+import Slide from "../../components/slide/Slide";
+import TrustedBy from "../../components/truestedBy/TrustedBy";
 import "./Home.scss";
 
 const Home = () => {
   return (
     <div className="home">
       <Featured />
+      <TrustedBy />
+      <Slide />
     </div>
   );
 };
