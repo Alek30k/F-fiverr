4a924b522136871c920696542dac87c35a509485
Alek30k <alemank23k@gmail.com>
2023-02-18 14:29:48 -0300

    last commit

--- src/pages/orders/Orders.jsx
+++ src/pages/orders/Orders.jsx
@@ -91,6 +91,23 @@ const Orders = () => {
               <img className="message" src="./img/message.png" alt="" />
             </td>
           </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Original ai generated digital art</td>
+            <td>
+              119.<sup>99</sup>
+            </td>
+            <td>Yoshi Tannamuri</td>
+            <td>
+              <img className="message" src="./img/message.png" alt="" />
+            </td>
+          </tr>
         </table>
       </div>
     </div>
