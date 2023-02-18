5148d8aeaaa52a088448233bcf971c7dc1a873af
Alek30k <alemank23k@gmail.com>
2023-02-18 14:29:34 -0300

    last commit

--- src/pages/orders/Orders.jsx
+++ src/pages/orders/Orders.jsx
@@ -74,6 +74,23 @@ const Orders = () => {
               <img className="message" src="./img/message.png" alt="" />
             </td>
           </tr>
+          <tr>
+            <td>
+              <img
+                className="image"
+                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+            </td>
+            <td>Illustration hyper realistic painting</td>
+            <td>
+              39.<sup>99</sup>
+            </td>
+            <td>Helen Bennett</td>
+            <td>
+              <img className="message" src="./img/message.png" alt="" />
+            </td>
+          </tr>
         </table>
       </div>
     </div>
