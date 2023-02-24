6af8dfbab405e97e024face06a94d2cebb35fc61
Alek30k <alemank23k@gmail.com>
2023-02-24 15:47:41 -0300

    last commit

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -14,10 +14,10 @@ function Login() {
     e.preventDefault();
     try {
       const res = await newRequest.post("/auth/login", { username, password });
-      //   localStorage.setItem("currentUser", JSON.stringify(res.data));
-      //   navigate("/");
+      localStorage.setItem("currentUser", JSON.stringify(res.data));
+      navigate("/");
     } catch (err) {
-      //   setError(err.response.data);
+      setError(err.response.data);
     }
   };
 
