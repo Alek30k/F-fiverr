350bfc84917362da9e334d2f3866818495e1e54d
Alek30k <alemank23k@gmail.com>
2023-02-24 22:43:36 -0300

    last commit

--- src/utils/upload.js
+++ src/utils/upload.js
@@ -3,10 +3,14 @@ import axios from "axios";
 const upload = async (file) => {
   const data = new FormData();
   data.append("file", file);
-  data.append("upload_preset", "fiverr");
+  data.append("upload_preset", "Mfiverr");
 
   try {
-    const res = await axios.post(import.meta.env.VITE_UPLOAD_LINK, data);
+    // const res = await axios.post(import.meta.env.VITE_UPLOAD_LINK, data);
+    const res = await axios.post(
+      "https://api.cloudinary.com/v1_1/dztl3rtlc/image/upload",
+      data
+    );
 
     const { url } = res.data;
     return url;
