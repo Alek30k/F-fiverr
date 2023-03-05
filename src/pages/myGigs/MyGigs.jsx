c4c672882c5c598ba673f48c96671279e7ce28b5
Alek30k <alemank23k@gmail.com>
2023-03-05 12:02:12 -0300

    last commit

--- src/pages/myGigs/MyGigs.jsx
+++ src/pages/myGigs/MyGigs.jsx
@@ -33,44 +33,55 @@ function MyGigs() {
 
   return (
     <div className="myGigs">
-      <div className="container">
-        <div className="title">
-          <h1>{currentUser.isSeller ? "Gigs" : "Orders"}</h1>
-          {currentUser.isSeller && (
-            <Link to="/add">
-              <button>Add New Gig</button>
-            </Link>
-          )}
-        </div>
-        <table>
-          <tr>
-            <th>Image</th>
-            <th>Title</th>
-            <th>Price</th>
-            <th>Sales</th>
-            <th>Action</th>
-          </tr>
-          {data.map((gig) => (
+      {isLoading ? (
+        "loading"
+      ) : error ? (
+        "error"
+      ) : (
+        <div className="container">
+          <div className="title">
+            <h1>Gigs</h1>
+            {currentUser.isSeller && (
+              <Link to="/add">
+                <button>Add New Gig</button>
+              </Link>
+            )}
+          </div>
+          <table>
             <tr>
-              <td>
-                <img
-                  className="image"
-                  src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                  alt=""
-                />
-              </td>
-              <td>Stunning concept art</td>
-              <td>
-                59.<sup>99</sup>
-              </td>
-              <td>13</td>
-              <td>
-                <img className="delete" src="./img/delete.png" alt="" />
-              </td>
+              <th>Image</th>
+              <th>Title</th>
+              <th>Price</th>
+              <th>Sales</th>
+              <th>Action</th>
             </tr>
-          ))}
-        </table>
-      </div>
+            {data.map((gig) => (
+              <tr>
+                <td>
+                  <img
+                    className="image"
+                    src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                    alt=""
+                  />
+                </td>
+                <td>Stunning concept art</td>
+                <td>
+                  59.<sup>99</sup>
+                </td>
+                <td>13</td>
+                <td>
+                  <img
+                    className="delete"
+                    src="./img/delete.png"
+                    alt=""
+                    onClick={() => handleDelete(gig._id)}
+                  />
+                </td>
+              </tr>
+            ))}
+          </table>
+        </div>
+      )}
     </div>
   );
 }
