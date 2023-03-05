bc1a8b07ebda3cf80235618a6d1a30f870f9d229
Alek30k <alemank23k@gmail.com>
2023-03-05 12:34:32 -0300

    last commit

--- src/reducers/gigReducer.js
+++ src/reducers/gigReducer.js
@@ -31,5 +31,15 @@ export const gigReducer = (state, action) => {
         ...state,
         features: [...state.features, action.payload],
       };
+    case "REMOVE_FEATURE":
+      return {
+        ...state,
+        features: state.features.filter(
+          (feature) => feature !== action.payload
+        ),
+      };
+
+    default:
+      return state;
   }
 };
