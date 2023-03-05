ab1897ed2661b66714e1203a6b4a2abc0d0ca0b8
Alek30k <alemank23k@gmail.com>
2023-03-05 11:47:55 -0300

    last commit

--- src/pages/myGigs/MyGigs.jsx
+++ src/pages/myGigs/MyGigs.jsx
@@ -18,6 +18,19 @@ function MyGigs() {
       }),
   });
 
+  const mutation = useMutation({
+    mutationFn: (id) => {
+      return newRequest.delete(`/gigs/${id}`);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(["myGigs"]);
+    },
+  });
+
+  const handleDelete = (id) => {
+    mutation.mutate(id);
+  };
+
   return (
     <div className="myGigs">
       <div className="container">
@@ -37,23 +50,25 @@ function MyGigs() {
             <th>Sales</th>
             <th>Action</th>
           </tr>
-          <tr>
-            <td>
-              <img
-                className="image"
-                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                alt=""
-              />
-            </td>
-            <td>Stunning concept art</td>
-            <td>
-              59.<sup>99</sup>
-            </td>
-            <td>13</td>
-            <td>
-              <img className="delete" src="./img/delete.png" alt="" />
-            </td>
-          </tr>
+          {data.map((gig) => (
+            <tr>
+              <td>
+                <img
+                  className="image"
+                  src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                  alt=""
+                />
+              </td>
+              <td>Stunning concept art</td>
+              <td>
+                59.<sup>99</sup>
+              </td>
+              <td>13</td>
+              <td>
+                <img className="delete" src="./img/delete.png" alt="" />
+              </td>
+            </tr>
+          ))}
         </table>
       </div>
     </div>
